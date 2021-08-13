08ec7df1d3a370e234ff5cc9ff6eb635d522ff66
joungwonlim <joungwonlim@gmail.com>
2021-08-14 08:58:23 +0900

    Server is running on port: 4000

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("mongoose");
+const app = express();
+
+require("dotenv-flow").config();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, function() {
+  console.log("Server is running on port: " + PORT);
+})
+
+module.exports = app;
