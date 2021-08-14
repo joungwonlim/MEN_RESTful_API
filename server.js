8292e198cfabc10b12ba6b90b400c399632e59dd
joungwonlim <joungwonlim@gmail.com>
2021-08-14 09:03:52 +0900

    MEN RESTful API - 5

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const app = express();
 
 require("dotenv-flow").config();
 
-const PORT = process.env.PORT || 4000;
+//routers
+app.get("/api/welcome", (req, res) => {
+  res.status(200).send({
+    message: "Welcome to the MEN RESTful API"
+  })
+});
 
+// 
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, function() {
   console.log("Server is running on port: " + PORT);
 })
