bf90a5bc6f1653d120f30fa31f5955aa7e358e7c
joungwonlim <joungwonlim@gmail.com>
2021-08-14 09:23:43 +0900

    MEN RESTful API - 07 - Connection to MongoDB

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ app.get("/api/welcome", (req, res) => {
   })
 });
 
+// mongoDB connect
+mongoose.connect(
+  process.env.DBHOST,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }
+).then((result) => {
+  console.log("Success connection to MongoDB")
+}).catch((error) => {
+  console.log("Error connection to MongoDb: " + error)
+})
+
+// mongoose.connection.once("open", () => {
+//   console.log("connected successful to MongoDB")
+// });
+
 // 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, function() {
